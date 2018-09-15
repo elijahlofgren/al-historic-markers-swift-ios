fc40e12023aba7067a6a8d3e07361c019a147695
Elijah Lofgren <elijahlofgren@gmail.com>
2018-09-15 15:15:56 -0500

    Got showing user's current location on embedded iOS map working by following stackoverflow post
    https://stackoverflow.com/a/33591128/908677

--- code/AL Historic Markers/AL Historic Markers/DetailViewController.swift
+++ code/AL Historic Markers/AL Historic Markers/DetailViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
     @IBOutlet weak var gpsLocationLabel: UILabel!
 
+    var locationManager: CLLocationManager!
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
@@ -21,12 +26,36 @@ class DetailViewController: UIViewController {
             }
         }
     }
+    
+    // Begin adapted from https://stackoverflow.com/a/33591128/908677
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: true)
+
+        
+    }
+    // End adapted from https://stackoverflow.com/a/33591128/908677
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         gpsLocationLabel.text = "test";
 
+        // Adapted from https://stackoverflow.com/a/33591128/908677
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
         
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
